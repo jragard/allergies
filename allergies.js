c72344f4199f1e1fa3b4a6436d98d13e07930926
Ryan Agard <jragard@gmail.com>
2019-06-14 19:27:36 -0400

    still need to refactor

--- allergies.js
+++ allergies.js
@@ -3,110 +3,90 @@ const patients = []
 
 // Allergy class takes 3 constructor properties for allergy name, severity, and the patient associated with the allergy.
 class Allergy {
-    constructor(allergy, severity, patientID) {
-      this.allergy = allergy;
-      this.severity = severity;
-      this.patientID = patientID;
-    }
+  constructor(allergy, severity) {
+    this.allergy = allergy;
+    this.severity = severity;
+  }
+}
+  
+// Patient class takes 2 constructor properties 'allergies', which will be a full list of Allergy instances, and 'id', which will be the patient's unique ID number
+class Patient {
+  constructor(id, name) {
+    this.allergies = [];
+    this.id = id;
+    this.name = name;
   }
   
-  // Patient class takes 2 constructor properties 'allergies', which will be a full list of Allergy instances, and 'id', which will be the patient's unique ID number
-  class Patient {
-    constructor(id, name) {
-      this.allergies = [];
-      this.id = id;
-      this.name = name;
-    }
-  
-    // filters the full list of allergy instances and returns only the allergies associated with this particular patient
-    getAllergies() {
-      let output = document.getElementById('lookupOutput');
+  getAllergies() {
+    let output = document.getElementById('lookupOutput');
       
-      if(this.allergies.length !== 0) {
-        output.innerHTML = JSON.stringify(this.allergies);
-        // let patientAllergies = this.allergies.filter(allergy => {
-        //   return allergy.patientID === this.id;
-        // });
-        // return patientAllergies
-      } else {
-        output.innerHTML = 'Patient has no allergies listed';
-      }
-    }
-  
-    // adds a list of one or more allergy instances to the patient's existing list of allergies
-    addAllergies(id) {
-      // let id = document.getElementById('newID').value;
-      let allergy = document.getElementById('allergy');
-      let severity = document.getElementById('severity');
-      let form = document.getElementById('addAllergy');
+    if(this.allergies.length !== 0) {
+      for(let i = 0; i < this.allergies.length; i++) {
+        let allergy = document.createElement('h5');
+        allergy.innerHTML = 'Allergy: ' + this.allergies[i].allergy + '--- ' + 'Severity: '  + this.allergies[i].severity;
+        let linebreak = document.createElement('br');
 
-      let newAllergy = new Allergy(allergy.value, severity.value, id);
-      this.allergies.push(newAllergy);
-      form.reset();
-      
+        output.append(allergy);
+        // output.append(linebreak);
+      }
+    } else {
+      output.innerHTML = 'Patient has no allergies listed';
     }
   }
+  
+  addAllergies(allergy, severity) {
+    let newAllergy = new Allergy(allergy, severity);
+    this.allergies.push(newAllergy);
+  }
+
+}
 
 const addAllergy = document.getElementById('addAllergy');
+const patientId = document.getElementById('newID');
+const allergy = document.getElementById('allergy');
+const severity = document.getElementById('severity');
+const allergyLookup = document.getElementById('lookup');
+const id = document.getElementById('patientId');
+const createPatient = document.getElementById('createPatient');
+const name = document.getElementById('name');
+const clearBtn = document.getElementById('clear');
+const addID = document.getElementById('id');
 
 addAllergy.onsubmit = (event) => {
   event.preventDefault();
 
-  let patientId = document.getElementById('newID').value;
-  console.log('patientId: ' + patientId);
-
   let patientIndex = patients.find(patient => {
-    return patient.id === patientId;
+    return patient.id === patientId.value;
   });
 
   patientIndex = patients.indexOf(patientIndex);
-  console.log(patientIndex);
-  console.log(patients);
+  patients[patientIndex].addAllergies(allergy.value, severity.value);
 
-  patients[patientIndex].addAllergies(patientId);
-  
-
+  patientId.value = '';
+  allergy.value = '';
+  severity.value = '';
 }
-
-const allergyLookup = document.getElementById('lookup');
 
 allergyLookup.onsubmit = (event) => {
   event.preventDefault();
-  // console.log('allergy lookup running');
-  // console.log(patients);
-  let id = document.getElementById('patientId');
+
   let queriedPatient = patients.filter(patient => {
     return patient.id === id.value;
   })[0];
-  // console.log(queriedPatient)
 
   queriedPatient.getAllergies();
+  id.value = '';
 }
-
-const createPatient = document.getElementById('createPatient');
 
 createPatient.onsubmit = (event) => {
   event.preventDefault();
-  let name = document.getElementById('name');
-  let id = document.getElementById('id');
-
-  patients.push(new Patient(id.value, name.value));
-  name.value = ''
-  id.value = '';
-  console.log(patients);
+  patients.push(new Patient(addID.value, name.value));
+  name.value = '';
+  addID.value = '';
 }
 
-const clearBtn = document.getElementById('clear');
-
 clearBtn.onclick = () => {
-  let lookup = document.getElementById('lookup');
-  let add = document.getElementById('addAllergy');
-  let createPatient = document.getElementById('createPatient');
   let output = document.getElementById('lookupOutput');
-
-  lookup.reset();
-  add.reset();
-  createPatient.reset();
   output.innerHTML = '';
 }
 
